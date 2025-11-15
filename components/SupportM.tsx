63ebac9cc627da09c6070000a0a25d3f6985deb4
NMaryko <sestrichkas@yandex.ru>
2025-11-15 18:16:50 +0400

    Create SupportM.tsx

---
+++ components/SupportM.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function SupportM() {
+  return (
+    <footer className="mt-48 pt-4 text-sm md:text-base text-[#5E3830] text-center">
+      <a href="mailto:support@medradix.info" className="font-medium">
+        support@medradix.info
+      </a>
+    </footer>
+  );
+}
